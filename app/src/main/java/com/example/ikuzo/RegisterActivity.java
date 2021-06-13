7ae00298de67c1176942c551b5a4a0bf70d38a01
varad <varadsoham555@gmail.com>
2021-06-14 00:14:10 +0530

    fixed writing to realtime database issue

--- app/src/main/java/com/example/ikuzo/RegisterActivity.java
+++ app/src/main/java/com/example/ikuzo/RegisterActivity.java
@@ -71,7 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
                             assert firebaseUser != null;
                             String userid = firebaseUser.getUid();
 
-                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+                            reference = FirebaseDatabase.getInstance("https://study-group-chat-room-2021-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users").child(userid);
 
                             HashMap<String,String> hashMap = new HashMap<>();
                             hashMap.put("id",userid);
